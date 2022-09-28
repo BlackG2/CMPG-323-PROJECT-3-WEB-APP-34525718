9da4e042ee5d723f2249a89bf00b8a18bbb13ccf
BlackG2 <generalmahotso33@gmail.com>
2022-09-28 16:11:28 +0200

    Last Commit

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository Intefaces/IDeviceRepository.cs
@@ -0,0 +1,10 @@
+﻿using DeviceManagement_WebApp.Controllers;
+using DeviceManagement_WebApp.Models;
+
+namespace DeviceManagement_WebApp.Repository_Intefaces
+{
+    public interface IDeviceRepository : IGenericRepository<Device>
+    {
+        Device GetMostRecentDevice();
+    }
+}
