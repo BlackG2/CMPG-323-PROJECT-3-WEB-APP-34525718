f15421334505ab63baea7fcbf24330661d383220
BlackG2 <generalmahotso33@gmail.com>
2022-09-28 15:24:04 +0200

    Added code

--- ConnectedOffice/DeviceManagement_WebApp/Repository Classes/GenericRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository Classes/GenericRepository.cs
@@ -1,6 +1,49 @@
-﻿namespace DeviceManagement_WebApp.Repository_Classes
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Repository_Intefaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DeviceManagement_WebApp.Repository_Classes
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        protected readonly ConnectedOfficeContext _context;
+
+        public GenericRepository(ConnectedOfficeContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public T GetById(int Id)
+        {
+            return _context.Set<T>().Find(Id);
+        }
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
     }
 }
--- ConnectedOffice/DeviceManagement_WebApp/Repository Intefaces/IGenericRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository Intefaces/IGenericRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace DeviceManagement_WebApp.Repository_Intefaces
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DeviceManagement_WebApp.Repository_Intefaces
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        T GetById(int Id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
